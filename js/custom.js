6e57993b6e143a1fc6b15cb512528572d9a04525
dagongdeyatou <869120041@qq.com>
2023-12-29 16:57:18 +0800

    Site updated: 2023-12-29 16:57:16

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 (function(a,h,g,f,e,d,c,b){b=function(){d=h.createElement(g);c=h.getElementsByTagName(g)[0];d.src=e;d.charset="utf-8";d.async=1;c.parentNode.insertBefore(d,c)};a["SeniverseWeatherWidgetObject"]=f;a[f]||(a[f]=function(){(a[f].q=a[f].q||[]).push(arguments)});a[f].l=+new Date();if(a.attachEvent){a.attachEvent("onload",b)}else{a.addEventListener("load",b,false)}}(window,document,"script","SeniverseWeatherWidget","//cdn.sencdn.com/widget2/static/js/bundle.js?t="+parseInt((new Date().getTime() / 100000000).toString(),10)));
-window.SeniverseWeatherWidget('show', {
+  window.SeniverseWeatherWidget('show', {
     flavor: "slim",
     location: "YB1UX38K6DY1",
     geolocation: true,
@@ -7,7 +7,6 @@ window.SeniverseWeatherWidget('show', {
     unit: "c",
     theme: "auto",
     token: "25f48209-fa4e-4d4b-b799-4cca6b3bfa75",
-    hover: "enabled",
+    hover: "disabled",
     container: "tp-weather-widget"
-})
-
+  })
